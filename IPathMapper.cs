3fc7ccb63578ddbf87f4b930303c059eeb2d4ba8
matt <matt@nerdamigo.com>
2015-07-20 21:17:50 -0400

    addition of IPathMapper

---
+++ IPathMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdAmigo.Abstractions
+{
+	public interface IPathMapper
+	{
+		string MapPath(string appRelativePath);
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NerdAmigo.Abstractions")]
+[assembly: AssemblyDescription("A collection of common abstractions for use throughout NerdAmigo projects.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NerdAmigo")]
+[assembly: AssemblyProduct("NerdAmigo.Abstractions")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b38ee05c-7cc7-4b44-9b13-f54d149d25ac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
